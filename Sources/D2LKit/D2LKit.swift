bdf949e3b3d959f6e3f85e05570203d1c37d6ada
Wouter01 <wouterhennen@gmail.com>
2022-06-23 04:22:53 +0200

    Initial Commit

---
+++ Sources/D2LKit/D2LKit.swift
@@ -0,0 +1,6 @@
+public struct D2LKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/D2LKitTests/D2LKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import D2LKit
+
+final class D2LKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(D2LKit().text, "Hello, World!")
+    }
+}
