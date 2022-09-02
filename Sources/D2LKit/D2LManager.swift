76802b4eb17ff4960fa71c8c344f0249d670a744
Wouter01 <wouterhennen@gmail.com>
2022-09-02 02:16:23 +0200

    improved login url build method

--- Sources/D2LKit/D2LManager.swift
+++ Sources/D2LKit/D2LManager.swift
@@ -9,35 +9,23 @@ import Foundation
 import CryptoKit
 import CommonCrypto
 
-
-
 public class D2LManager {
     public static var shared = D2LManager()
     public var builder: D2LUserRequest?
     
-    
-    
-    public static func createLoginURL(appId: String, appKey: String, baseURLPort: Int = 443, baseURL: URL, callbackURL: URL, buildURL: (CharacterSet, String) -> URL) -> Result<URL, AuthError> {
-        
-        var cs = CharacterSet()
-        cs.formUnion(.urlHostAllowed)
-        cs.insert("-")
-        cs.remove(":")
-        cs.remove("=")
-        cs.remove("&")
+    public static func buildLoginURL(appID: String, appKey: String, baseURL: URL, callbackURL: URL) throws -> URL {
         
         let hmacKey = createHMAC(key: appKey, data: callbackURL.absoluteString)
+        let encodedURL = callbackURL.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
         
-        guard let callbackURLEncoded = callbackURL.absoluteString.addingPercentEncoding(withAllowedCharacters: cs) else {
-            return .failure(.callbackURLError)
+        let url = URL(string: "\(baseURL.absoluteString)/d2l/auth/api/token?x_b=\(hmacKey)&x_target=\(encodedURL)&type=mobile&x_a=\(appID)")
+        
+        guard let url else {
+            throw URLError(.badURL)
         }
         
-        let test = "/d2l/auth/api/token?x_b=\(hmacKey)&x_target=\(callbackURLEncoded)&type=mobile&x_a=\(appId)"
-        
-        return .success(buildURL(cs, test))
+        return url
     }
-    
-    
     
     /// Convert a Base 64 encoded string to the [https://datatracker.ietf.org/doc/html/rfc4648.html](RFC 4648) format.
     /// Specifically, '=' characters are removed, and '+' and '/' characters are replaced by '-' and '_'.
@@ -58,7 +46,6 @@ public class D2LManager {
     ///   - data: The callback url of the application.
     static func createHMAC(key: String, data: String) -> String {
         let hmacDigest = HMAC<SHA256>.authenticationCode(for: Data(data.utf8), using: .init(data: Data(key.utf8)))
-        
         let base64Encoded = String(data: Data(hmacDigest).base64EncodedData(), encoding: .utf8)!
         return convertBase64String(data: base64Encoded)
     }
